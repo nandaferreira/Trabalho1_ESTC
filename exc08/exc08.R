b9120418be7dc79ff45567591bde05daa3a537d9
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-09-04 15:10:14 -0300

    Add files via upload

---
+++ exc08/exc08.R
@@ -0,0 +1,21 @@
+#Questão 8: Michael Scott é gerente regional da empresa Dunder Mufflin. Para as festividades de fim de ano, Michael propôs aos funcionários Dwight Schrute, Jim Halpert, Kevin Malone e Creed Bratton a realização de um amigo oculto entre eles. Consideraremos que o sorteio do amigo oculto deu errado quando uma pessoa sortear ela mesma (Michael tira Michael, por exemplo). Simule o sorteio do amigo oculto. Se ele deu certo, atribua o valor 1; caso contrário, atribua o valor 0 (zero). Em seguida, replique este experimento cem mil vezes e calcule a proporção de vezes que o amigo oculto deu errado.
+
+
+participantes <- c("Michael", "Dwight", "Kevin", "Jim", "Creed")
+
+amigo_oculto <- function(){
+  sorteio <- sample(participantes)
+  if(any(sorteio == participantes)){
+    return (0)
+  }else{
+    return(1)
+  }
+}
+
+set.seed(150)
+resultado <- replicate(100000, amigo_oculto())
+
+
+head(resultado, 20)
+proporcao_errado <- mean(resultado)
+proporcao_errado
