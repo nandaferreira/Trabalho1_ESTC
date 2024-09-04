d6351c96c218b37fae246c099fc590aeab8b0dda
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-09-04 15:10:54 -0300

    Update exc08.R

--- exc08/exc08.R
+++ exc08/exc08.R
@@ -1,5 +1,8 @@
-#Questão 8: Michael Scott é gerente regional da empresa Dunder Mufflin. Para as festividades de fim de ano, Michael propôs aos funcionários Dwight Schrute, Jim Halpert, Kevin Malone e Creed Bratton a realização de um amigo oculto entre eles. Consideraremos que o sorteio do amigo oculto deu errado quando uma pessoa sortear ela mesma (Michael tira Michael, por exemplo). Simule o sorteio do amigo oculto. Se ele deu certo, atribua o valor 1; caso contrário, atribua o valor 0 (zero). Em seguida, replique este experimento cem mil vezes e calcule a proporção de vezes que o amigo oculto deu errado.
-
+#Questão 8: Michael Scott é gerente regional da empresa Dunder Mufflin. Para as festividades de fim de ano, Michael propôs aos 
+#funcionários Dwight Schrute, Jim Halpert, Kevin Malone e Creed Bratton a realização de um amigo oculto entre eles. 
+#Consideraremos que o sorteio do amigo oculto deu errado quando uma pessoa sortear ela mesma (Michael tira Michael, 
+#por exemplo). Simule o sorteio do amigo oculto. Se ele deu certo, atribua o valor 1; caso contrário, atribua o valor 0 (zero). 
+#Em seguida, replique este experimento cem mil vezes e calcule a proporção de vezes que o amigo oculto deu errado.
 
 participantes <- c("Michael", "Dwight", "Kevin", "Jim", "Creed")
 
