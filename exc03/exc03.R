d3419ea38d397b7c9aca2db20fab84c2eac8bd59
Fernanda Ferreira <114786782+nandaferreira@users.noreply.github.com>
2024-09-19 16:32:05 -0300

    corrigido

--- exc03/exc03.R
+++ exc03/exc03.R
@@ -2,22 +2,22 @@
 
 #(a)Somatório índice(n) = 20, m = 30
 
+somatorio1 <- c()
 
-somatorio1 <- 0
-resultado1 <- 0
 for (n in 20:30){
-  somatorio1 <- ((n^2) + 4*n)
-  resultado1 <- sum(somatorio1)
-  
+  somatorio1 <- c(somatorio1,((n^2) + 4*n))
 }
-print(resultado1)
+resultado1 <- sum(somatorio1)
 
-#(b) Somatório índice(n) = 10, m = 20 **REVER
+#(b) Somatório índice(n) = 10, m = 20
 
-somatorio2 <- 0
-resultado2 <- 0
-
+somatorio2 <- c()
 for(k in 10:20){
-  somatorio2 <-(((3^k)/k) +((2^k)/(k^2)))
+  somatorio2 <-c(somatorio2,(((3^k)/k) +((2^k)/(k^2))))
 }
-somatorio2
+resultado2 <- sum(somatorio2)
+
+
+#OBS
+#somatorio1  é um vetor que guarda o resultado da equação dada até o fim do intervalo
+#o resultado final é o somatório de todos esses valores
